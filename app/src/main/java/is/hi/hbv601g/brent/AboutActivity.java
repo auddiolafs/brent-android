a74fc7f9dab4f9eb916e19c09763f3915574ad42
auddiolafs <aao7@hi.is>
2019-02-12 19:06:01 +0000

    Resolving minor errors

--- app/src/main/java/is/hi/hbv601g/brent/AboutActivity.java
+++ app/src/main/java/is/hi/hbv601g/brent/AboutActivity.java
@@ -7,42 +7,43 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+
 // Check comments in MainActivity for explanation
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends CurrentActivity implements FetchTask.FetchTaskCallback {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Basic things which are done in each activity
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settings:
-                // TODO
-                return true;
-
-            case R.id.home:
-               // TODO
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
+        if(this.connected) {
+            setUp();
         }
     }
+
+    public void setUp() {
+
+        setContentView(R.layout.activity_about);
+        // Get toolbar in layout (defined in xml file)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Set it as actionbar
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        // Set "go back" functionality
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
+        // Remove label/projectName/title from actionbar
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public void onResultReceived(JSONArray result) {
+        //System.out.println(result);
+    }
+
 
 }
