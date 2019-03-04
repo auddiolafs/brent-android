a61ae50307becc61b42785247cf7241f80e73820
ragnaolafs <rao9@hi.is>
2019-03-04 22:07:33 +0000

    working on BookingService

--- app/src/main/java/is/hi/hbv601g/brent/BookingService.java
+++ app/src/main/java/is/hi/hbv601g/brent/BookingService.java
@@ -1,4 +1,32 @@
 package is.hi.hbv601g.brent;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+import java.util.Date;
+
 public class BookingService {
+
+    FirebaseApp mApp;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "BookingService";
+
+    public BookingService() {
+        mApp = FirebaseApp.getInstance();
+        mAuth = FirebaseAuth.getInstance(mApp);
+        FirebaseUser user = mAuth.getCurrentUser();
+    }
+
+    public void saveBooking(List<Bike> bikes, List<Accessory> accessories, List<Tour> tours,
+                            Date startDate, Date endDate, String pickupLocation) {
+
+    }
+
+    public void cancelBooking(String bookingId) {
+
+    }
 }
