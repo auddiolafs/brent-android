aa4cd92b62652c8f681d52215454ca97f906021b
ragnaolafs <rao9@hi.is>
2019-03-06 13:54:05 +0000

    working on saveBooking

--- app/src/main/java/is/hi/hbv601g/brent/BookingService.java
+++ app/src/main/java/is/hi/hbv601g/brent/BookingService.java
@@ -2,28 +2,31 @@ package is.hi.hbv601g.brent;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.List;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BookingService {
 
     FirebaseApp mApp;
     FirebaseAuth mAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String userId;
     private static final String TAG = "BookingService";
 
     public BookingService() {
         mApp = FirebaseApp.getInstance();
         mAuth = FirebaseAuth.getInstance(mApp);
-        FirebaseUser user = mAuth.getCurrentUser();
-
+        userId = mAuth.getCurrentUser().getUid();
     }
 
     public void saveBooking(List<Bike> bikes, List<Accessory> accessories, List<Tour> tours,
                             Date startDate, Date endDate, String pickupLocation) {
+
+        Map<String, Object> booking = new HashMap<>();
 
     }
 
