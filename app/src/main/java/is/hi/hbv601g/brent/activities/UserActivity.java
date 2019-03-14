922891d9490db96c72f0c1a0d07aca455a0bdace
dingolfsson <dai5@hi.is>
2019-03-14 21:25:57 +0000

    User Page

--- app/src/main/java/is/hi/hbv601g/brent/activities/UserActivity.java
+++ app/src/main/java/is/hi/hbv601g/brent/activities/UserActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +20,7 @@ public class UserActivity extends CurrentActivity {
     ImageButton toolbarHome;
     ImageButton toolbarCart;
 
-    TextView mLogoutEdit;
+    Button mLogoutButton;
 
     FirebaseAuth mAuth;
     FirebaseApp mApp;
@@ -65,9 +66,9 @@ public class UserActivity extends CurrentActivity {
             }
         });
 
-        mLogoutEdit = findViewById(R.id.logoutText);
+        mLogoutButton = findViewById(R.id.logoutButton);
 
-        mLogoutEdit.setOnClickListener(new View.OnClickListener() {
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
