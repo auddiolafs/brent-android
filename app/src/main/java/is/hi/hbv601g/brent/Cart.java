2be5b59776580584dcdd5be59e877e9ff31e379a
ragnaolafs <rao9@hi.is>
2019-04-02 18:03:43 +0000

    removed unused code

--- app/src/main/java/is/hi/hbv601g/brent/Cart.java
+++ app/src/main/java/is/hi/hbv601g/brent/Cart.java
@@ -50,16 +50,6 @@ public class Cart {
         return mCart;
     }
 
-    public void putBooking(Booking booking) {
-        mBookings.put(mCartID, booking);
-        mCartID += 1;
-    }
-
-    public void removeBooking(Long bookingId) {
-        mBookings.remove(bookingId);
-        mCartID -= 1;
-    }
-
     public boolean contains(Date startDate, Date endDate) {
         Iterator it = mBookings.entrySet().iterator();
         while (it.hasNext()) {
@@ -69,54 +59,6 @@ public class Cart {
             if (booking.getStartDate() == startDate && booking.getEndDate() == endDate) return true;
         }
         return false;
-    }
-
-    public Booking popBooking(Date startDate, Date endDate) {
-        Iterator it = mBookings.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            String key = pair.getKey().toString();
-            Booking booking = (Booking) pair.getValue();
-            if (booking.getStartDate() == startDate && booking.getEndDate() == endDate)
-                return booking;
-        }
-        return new Booking();
-    }
-
-    public void saveCart() {
-        Iterator it = mBookings.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            Booking booking = (Booking) pair.getValue();
-            saveBookingToFirebase(booking);
-            removeBooking(mCartID);
-        }
-    }
-
-    private void saveBookingToFirebase(Booking booking) {
-        Map<String, Object> data = new HashMap<>();
-        Log.d("Cart.java", "Enter saveBooking");
-        data.put("startDate", booking.getStartDate());
-        data.put("endDate", booking.getEndDate());
-        data.put("pickupLocation", booking.getPickupLocation());
-        mDB.collection("bookings")
-                .add(data)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
-                        FirebaseAuth auth = FirebaseAuth.getInstance(FirebaseApp.getInstance());
-                        FirebaseUser user = auth.getCurrentUser();
-                        documentReference.update("user", user.getUid());
-                        // Get bike, tours and accessories and add
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
     }
 
     public void resetCart() {
